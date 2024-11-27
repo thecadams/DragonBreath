21c8ff91858e600be7255d076f4ca9e9153e4203
Chris Adams <chris@cadams.com.au>
2024-11-27 17:32:01 +1100

    Initial Commit

---
+++ DragonBreath/DragonBreathApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBreathApp.swift
+//  DragonBreath
+//
+//  Created by Chris Adams on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DragonBreathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
